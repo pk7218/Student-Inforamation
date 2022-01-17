b7b6c65f17ed8ebe0a67901d0cb33a0b88b79566
Pramod Kumbhar <kumbharpramod30@gmail.com>
2022-01-18 02:36:21 +0530

    correction commit

--- src/TrialDbcon.java
+++
@@ -1,9 +0,0 @@
-
-public class TrialDbcon
-{
-   public static void main(String[] args) {
-	DbConnection d=new DbConnection();
-	d.Insert("insert into student_master values(null,'tytyty','tytyty','male','1998-07-01',now())");
-	System.out.println("done");
-}
-}
